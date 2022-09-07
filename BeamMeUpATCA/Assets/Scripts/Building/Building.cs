43fb4aa903d49ed4681ef3c0b78445f4cb31ec8b
JacksonAnthon <jackson@anthon.net>
2022-09-07 16:10:15 +1000

    Some modifications to the Building script, adding a very basic extension for the array, and duplicating the tick damage functionality for a morale property on Units.

--- BeamMeUpATCA/Assets/Scripts/Building/Building.cs
+++ BeamMeUpATCA/Assets/Scripts/Building/Building.cs
@@ -8,15 +8,20 @@ namespace BeamMeUpATCA
     {
         // Setup
         [SerializeField]
+        private float maxHealth;
+
+        [SerializeField]
         private float healthPool;
         [SerializeField]
-        private float tickDmg; // We could make the engineers temporarily change tickDmg to a positive value
+        private float tickDmg; // We could make the engineers temporarily change tickDmg to a negative value
         [SerializeField]
         private bool isBroken;
         private float tickCounter;
 
+        private Queue<Unit> storedUnits;
 
         private void Awake() {
+            maxHealth = 100f;
             healthPool = 100f;
             tickDmg = 1f;
             isBroken = false;
@@ -24,7 +29,8 @@ namespace BeamMeUpATCA
         }
 
         private void FixedUpdate() {
-            if(!isBroken) {takeTickDamage();}
+            //If it's not broken, or if it is broken and is being healed
+            if(!isBroken || tickDmg < 0) {takeTickDamage();}
         }
 
         private void takeTickDamage() {
@@ -34,6 +40,11 @@ namespace BeamMeUpATCA
                 if(newHp <= 0) {
                     newHp = 0;
                     isBroken = true;
+                } else if (isBroken) {
+                    isBroken = false; //if being healed from broken, fix
+                }
+                if(newHp >= maxHealth) {
+                    newHp = maxHealth; //do not heal over full
                 }
                 healthPool = newHp;
                 tickCounter = 0;
@@ -49,6 +60,22 @@ namespace BeamMeUpATCA
 
         public void setBreak(bool flag) {
             isBroken = flag;
+        }
+
+        public float getHealth() {
+            return healthPool;
+        }
+
+        public Queue<Unit> getUnits() {
+            return storedUnits;
+        }
+
+        public void storeUnit(Unit unit) {
+            storedUnits.Enqueue(unit);
+        }
+
+        public Unit removeUnit() {
+            return storedUnits.Dequeue();
         }
     }
 }
