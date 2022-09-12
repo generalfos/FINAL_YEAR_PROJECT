b78ca774021394bffa41a8e42ce41f2eee8c3590
Joel Foster <generalfos@gmail.com>
2022-09-12 21:50:23 +1000

    Documentation and cleaning up code.

--- BeamMeUpATCA/Assets/Scripts/TutorialSequencer.cs
+++ BeamMeUpATCA/Assets/Scripts/TutorialSequencer.cs
@@ -1,3 +1,14 @@
+/** 
+ * TutorialSequencer.cs
+ * 
+ * This script handles the overall tutorial process from information popups
+ * to instructing users what they should do next. Behaves as the macro organiser
+ * of in game events during the tutorial scene.
+ * 
+ * @author: Joel Foster
+ * @last_edited: 12/09/2022 21:38
+ */
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,20 +16,21 @@ namespace BeamMeUpATCA
 {
     public class TutorialSequencer : MonoBehaviour
     {
-        // Link below for information on MonoBehaviour
-        // https://docs.unity3d.com/2020.3/Documentation/ScriptReference/MonoBehaviour.html
-
+        // External references to game objects necessary for tutorial progress.
         [field: SerializeField] public GameObject TutPopUpPrefab { get; private set; }
         [field: SerializeField] public GameObject Camera { get; private set; }
         [field: SerializeField] public GameObject Engineer { get; private set; }
+
         // Position popup directly in centre of screen
-        private Vector3 screenCenter = new Vector3(Screen.width/2, Screen.height/2, 0);
+        static private Vector3 screenCenter = new Vector3(Screen.width/2, Screen.height/2, 0);
         private GameObject currPopUp;
+        private int tutSeqNo;
+        private bool popUpActive;
+
+        // TODO: Introduce serialised fields for these elements or another more appropriate method
         static private int titleIndex = 0;
         static private int contentIndex = 1;
         static private int btnIndex = 2;
-        private int tutSeqNo;
-        private bool popUpActive;
 
         // Awake is init function. Start before first frame
         private void Awake() {
@@ -27,33 +39,39 @@ namespace BeamMeUpATCA
             Intro();
         }
 
+        // Tear down prompts after use
         private void DestroyPrompt(GameObject prompt)
         {
             Destroy(prompt);
         }
 
+        // Temporarily hide prompts
         static private void HidePrompt(GameObject prompt)
         {
             prompt.SetActive(false);
         }
 
+        // Unhide prompt
         static private void ShowPrompt(GameObject prompt)
         {
             prompt.SetActive(true);
         }
 
+        // General method for instantiating a new prompt
         private void CreateNewPrompt(string title, string content)
         {
-            // Only one active popup allowed at any given time
+            // Only allow one active popup at any given time
             if (currPopUp)
             {
                 DestroyPrompt(currPopUp);
                 currPopUp = null;
             }
             GameObject newPrompt = Instantiate(TutPopUpPrefab, screenCenter, Quaternion.identity, gameObject.transform);
+            // Get references to new text fields and button
             Text Title = newPrompt.transform.GetChild(0).GetChild(titleIndex).GetComponentInChildren<Text>();
             Text Content = newPrompt.transform.GetChild(0).GetChild(contentIndex).GetComponentInChildren<Text>();
             Button btn = newPrompt.transform.GetChild(0).GetChild(btnIndex).GetComponentInChildren<Button>();
+            // Set text and button listner
             Title.text = title;
             Content.text = content;
             btn.onClick.AddListener(HandleOk);
@@ -61,6 +79,7 @@ namespace BeamMeUpATCA
             popUpActive = true;
         }
         
+        // Resolver for Tutorial Ok clicked
         private void HandleOk()
         {
             Debug.Log("Button Clicked");
@@ -70,6 +89,7 @@ namespace BeamMeUpATCA
             tutSeqNo += 1;
         }
 
+        // Welcome message
         private void Intro()
         {
             CreateNewPrompt("Welcome to the Beam Me Up ATCA Tutorial",
@@ -77,6 +97,7 @@ namespace BeamMeUpATCA
                 " Betelguese");
         }
 
+        // Introducing engineers
         private void EngineerTut()
         {
             Camera.transform.LookAt(Engineer.transform);
@@ -85,6 +106,7 @@ namespace BeamMeUpATCA
                 "\nSelect the engineer now by left clicking the unit with the mouse hovering over it. ");
         }
 
+        // Update to next tutorial stage on completion of a stage
         private void Update()
         {
             if (!popUpActive)
