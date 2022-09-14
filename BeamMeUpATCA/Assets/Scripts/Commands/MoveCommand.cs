81b4952d5cbdbb7439e951020f0553611ebeaba7
JaydenBNE <jayden.howard@uqconnect.edu.au>
2022-09-14 11:56:09 +1000

    refactored player.cs to allow easier subscription of inputactions to actions. tweaked some UI. Removed some zombie code

--- BeamMeUpATCA/Assets/Scripts/Commands/MoveCommand.cs
+++ BeamMeUpATCA/Assets/Scripts/Commands/MoveCommand.cs
@@ -17,28 +17,28 @@ namespace BeamMeUpATCA
 
         public override void Execute(Unit unit) 
         {
-            RayCast = Camera.main.ScreenPointToRay(_player.Pointer.ReadValue<Vector2>());
-            if (!Physics.Raycast(RayCast, out RayCastHit, 10000f))
-            {
-                Debug.Log("No hit");
-                return;
-            }
-            string RayCastHitTag = RayCastHit.transform.gameObject.tag;
-            Debug.Log(RayCastHitTag);
-            if (RayCastHitTag == "ConfigSlot")
-            {
-                if (unit.UnitClass == Unit.UnitType.Array)
-                {
-                    unit.gameObject.transform.position = RayCastHit.point;
-                }
-            } else
-            {
-                if (!(unit.UnitClass == Unit.UnitType.Array))
-                {
-                    unit.gameObject.transform.position = RayCastHit.point;
-                }
-            }
-            Debug.Log("MoveCommand called");
+            // RayCast = Player.main.ScreenPointToRay(_player.Pointer.ReadValue<Vector2>());
+            // if (!Physics.Raycast(RayCast, out RayCastHit, 10000f))
+            // {
+            //     Debug.Log("No hit");
+            //     return;
+            // }
+            // string RayCastHitTag = RayCastHit.transform.gameObject.tag;
+            // Debug.Log(RayCastHitTag);
+            // if (RayCastHitTag == "ConfigSlot")
+            // {
+            //     if (unit.UnitClass == Unit.UnitType.Array)
+            //     {
+            //         unit.gameObject.transform.position = RayCastHit.point;
+            //     }
+            // } else
+            // {
+            //     if (!(unit.UnitClass == Unit.UnitType.Array))
+            //     {
+            //         unit.gameObject.transform.position = RayCastHit.point;
+            //     }
+            // }
+            // Debug.Log("MoveCommand called");
 
         }
     }
