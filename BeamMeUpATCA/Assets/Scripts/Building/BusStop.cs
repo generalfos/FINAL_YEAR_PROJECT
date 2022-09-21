e0cd4dd034d1269c9256ab7fbca09611abfb607a
tommiboy17 <tomdonaldson79@gmail.com>
2022-09-21 21:28:25 +1000

    Building now an abstract class that only holds anchor info; BusStop changed to inherit Enterable and Workable; Methods added to Enterable; Mendable inherits from Building

--- BeamMeUpATCA/Assets/Scripts/Building/BusStop.cs
+++ BeamMeUpATCA/Assets/Scripts/Building/BusStop.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 namespace BeamMeUpATCA
 {
 
-    public class BusStop : MonoBehaviour, Workable
+    public class BusStop : Building, Enterable, Workable
     {
         private List<Unit> inTown = new List<Unit>();
 
-        // Update is called once per frame
         void Update()
         {
             // Check each the inTownCounter for each unit in inTown
@@ -17,8 +16,9 @@ namespace BeamMeUpATCA
             {
                 if (u.getInTownCounter() == 0)
                 {
-                    inTown.Remove(u);
+                    Leave(u);
                     // Show the unit
+                    //TODO Now that work() explicitly requires entering a building, this should be changed
                     u.GetComponent<Renderer>().enabled = true;
                 }
             }
@@ -26,11 +26,44 @@ namespace BeamMeUpATCA
         
         /*
          * Adds a unit to the in town list
+         * This _should not_ be called directly for bus stop - Call Work() instead
+         */
+        public void Enter(Unit unit)
+        {
+            inTown.Add(unit);
+            // TODO Hide unit and place an icon above the bus stop
+        }
+
+        /*
+         * Return true is a specific unit is in town, else false
+         */
+        public bool IsInside(Unit unit)
+        {
+            if (inTown.Contains(unit))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /*
+        * Removes a unit to the in town list
+        * This _should not_ be called directly for bus stop - Call Work() instead
+        */
+        public void Leave(Unit unit)
+        {
+            inTown.Remove(unit);
+        }
+        
+        
+        /*
+         * This is what gets called when we want to send a unit to town
          */
         public void Work(Unit unit)
         {
-            inTown.Add(unit);
+            Enter(unit);
             // Hide the unit
+            //TODO Now that work() explicitly requires entering a building, this should be changed
             unit.GetComponent<Renderer>().enabled = false;
         }
         
--- BeamMeUpATCA/Assets/Scripts/Building/Interfaces/Enterable.cs
+++ BeamMeUpATCA/Assets/Scripts/Building/Interfaces/Enterable.cs
@@ -1,8 +1,14 @@
+using System.Collections.Generic;
+
 namespace BeamMeUpATCA
 {
     public interface Enterable
     {
         // Takes a Unit and handles it entering the implementing class.
         void Enter(Unit unit);
+        bool IsInside(Unit unit);
+        void Leave(Unit unit);
     }
+    
+    
 }
