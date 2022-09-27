d214addb6d41a3489af6fd235b6c855a9df1d567
JaydenBNE <jayden.howard@uqconnect.edu.au>
2022-09-27 18:36:31 +1000

    Implemented Mend Command

--- BeamMeUpATCA/Assets/Scripts/Commands/MendCommand.cs
+++ BeamMeUpATCA/Assets/Scripts/Commands/MendCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace BeamMeUpATCA
@@ -13,20 +14,64 @@ namespace BeamMeUpATCA
             Name = "Mend";
         }
 
-        // Check Command conditions. If conditions met but the unit is not 
-        // at the building dock, call Pathfinder - Goto(Camera, Vector2)
-        // Check in Update() for (IsGotoFinished && CommandConditions).
-        // Ensure methods respect the expected call count (single vs multiple calls)
-        // of the building interface methods.
-        public override void Execute() { base.Execute(); }
+        private bool conditionsMet = true;
+        Mendable building = null;
+
+        public override void Execute() 
+        {
+            GameObject selectedObject = Selector.SelectGameObject(ActiveCamera, Position, new[]{"Building"});
+
+            // Try to get building assuming selected object is not null.
+            try { building = selectedObject.GetComponent<Mendable>();
+            } catch (NullReferenceException) 
+            {
+                Debug.Log("Building object is not mendable");
+                building = null;
+            }
+
+            // The selected object is not a mendable building therefor abort command.
+            if (selectedObject == null || building == null) 
+            { 
+                Debug.Log("found no mendable building");
+                conditionsMet = false; 
+                return; 
+            }
+
+            if (building.Anchors.CanAnchor(unit.transform.position))
+            {
+                building.Mend(unit);
+            } 
+            else 
+            {
+                Vector3 position = building.Anchors.GetAnchorPoint();
+                Vector2 positionTemp = new Vector2(position.x, position.y);
+                Goto(ActiveCamera, ActiveCamera.WorldToScreenPoint(position));
+            }
+        }
 
         // Update for loop per frame. FixedUpdate for loop per physics step.
         // Update() counts in Time.deltaTime. FixedUpdate counts in Time.fixedDeltaTime.
-        private void Update() {}
-        private void FixedUpdate() {}
+        private void Update() 
+        {
+            if (building == null) Debug.Log("null?");
+            if (building != null)
+            {
+                Debug.Log("Building: not null");
+                if (building.Anchors.CanAnchor(unit.transform.position))
+                {
+                    Debug.Log("Anchor: can anchor");
+                    building.Mend(unit);
+                } 
+            }
+        }
 
         // Should return true if the command has finished execution. Goal condition.
         // Consider adding a timeout to the command if it doesn't have an guaranteed end state.
-        public override bool IsFinished() { return base.IsFinished(); }
+        public override bool IsFinished() 
+        {
+            if (!conditionsMet) return true;
+            if (building != null) return building.isRepaired;
+            return false;
+        }
     }
 }
