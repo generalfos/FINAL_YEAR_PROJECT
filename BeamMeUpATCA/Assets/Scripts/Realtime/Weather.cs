98d2f10117797f1a735d53d28d2c9d64ab0f92e2
Joel Foster <generalfos@gmail.com>
2022-10-04 22:12:51 +1000

    Initial weather retrieval and better tutorial data storage in json files.

--- BeamMeUpATCA/Assets/Scripts/Realtime/Weather.cs
+++ BeamMeUpATCA/Assets/Scripts/Realtime/Weather.cs
@@ -1,32 +1,120 @@
+/** 
+ * Weather.cs
+ * 
+ * This script handles retrieving the latest weather data utilising the BOM api
+ * and then modifying the game state to reflect this.
+ * 
+ * @author: Joel Foster
+ * @last_edited: 4/10/2022 22:03
+ */
+
 using UnityEngine;
 using UnityEngine.Networking;
+using System;
 using System.Collections;
-using System.Text.RegularExpressions;
-using System.Xml;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+// using System.Text.RegularExpressions;
 
 namespace BeamMeUpATCA
 {
+    public class Datum
+    {
+        public int sort_order;
+        public int wmo;
+        public string name;
+        public string history_product;
+        public string local_date_time;
+        public string local_date_time_full;
+        public string aifstime_utc;
+        public double lat;
+        public double lon;
+        public double apparent_t;
+        public string cloud;
+        public int? cloud_base_m;
+        public int? cloud_oktas;
+        public string cloud_type;
+        public object cloud_type_id;
+        public double delta_t;
+        public int gust_kmh;
+        public int gust_kt;
+        public double air_temp;
+        public double dewpt;
+        public double press;
+        public double press_msl;
+        public double press_qnh;
+        public string press_tend;
+        public string rain_trace;
+        public int rel_hum;
+        public string sea_state;
+        public string swell_dir_worded;
+        public object swell_height;
+        public object swell_period;
+        public string vis_km;
+        public string weather;
+        public string wind_dir;
+        public int wind_spd_kmh;
+        public int wind_spd_kt;
+    }
+
+    public class Header
+    {
+        public string refresh_message;
+        public string ID;
+        public string main_ID;
+        public string name;
+        public string state_time_zone;
+        public string time_zone;
+        public string product_name;
+        public string state;
+    }
+
+    public class Notice
+    {
+        public string copyright;
+        public string copyright_url;
+        public string disclaimer_url;
+        public string feedback_url;
+    }
+
+    public class Observations
+    {
+        public List<Notice> notice;
+        public List<Header> header;
+        public List<Datum> data;
+    }
+
+    public class ObservationsRoot
+    {
+        public Observations observations;
+    }
+
+
     public class Weather : MonoBehaviour
     {
         // Link below for information on MonoBehaviour
         // https://docs.unity3d.com/2020.3/Documentation/ScriptReference/MonoBehaviour.html
 
-        const string table_pattern = "<table.*?>(.*?)</table>";
-        const string tr_pattern = "<tr>(.*?)</tr>";
-        const string td_pattern = "<td.*?>(.*?)</td>";
-        private Regex regex = new Regex(@"Australia Telescope (Culgoora)", RegexOptions.IgnoreCase);
+        // const string table_pattern = "<table.*?>(.*?)</table>";
+        // const string tr_pattern = "<tr>(.*?)</tr>";
+        // const string td_pattern = "<td.*?>(.*?)</td>";
+        // private Regex regex = new Regex(@"Australia Telescope (Culgoora)", RegexOptions.IgnoreCase);
+        [SerializeField]
+        TextAsset jsonData;
 
-        // Awake is init function. Start before first frame
         private void Awake() 
         {
             Debug.Log("Weather start");
-            // StartCoroutine(weatherGetRequest("https://ozforecast.com.au/cgi-bin/weatherstation.cgi?station=11001"));
+            // http://www.bom.gov.au/fwo/IDN60801/IDN60801.95734.json
+            // https://ozforecast.com.au/cgi-bin/weatherstation.cgi?station=11001
+            StartCoroutine(weatherGetRequest("http://www.bom.gov.au/fwo/IDN60801/IDN60801.95734.json"));
         }
 
         private IEnumerator weatherGetRequest(string url)
         { 
             using (UnityWebRequest req = UnityWebRequest.Get(url))
             {
+                req.SetRequestHeader("user-agent", "");
                 yield return req.SendWebRequest();
 
                 switch (req.result)
@@ -35,10 +123,12 @@ namespace BeamMeUpATCA
                         Debug.Log(":\nReceived: " + req.downloadHandler.text);
                         break;
                     default:
-                        Debug.Log("Request Error");
+                        Debug.LogError("Request Error; " + req.result);
                         break;
                 }
-                var doc = req.downloadHandler.data;
+                string jsonString = req.downloadHandler.text;
+                var record = JsonConvert.DeserializeObject<ObservationsRoot>(jsonString);
+                Debug.Log(record);
                 // var elem = doc.GetElementById("ozf");
                 // Debug.Log(elem);
                 // var captured_text = regex.Match(doc);
--- BeamMeUpATCA/Assets/Scripts/Tutorial/TutorialSequencer.cs
+++ BeamMeUpATCA/Assets/Scripts/Tutorial/TutorialSequencer.cs
@@ -6,17 +6,60 @@
  * of in game events during the tutorial scene.
  * 
  * @author: Joel Foster
- * @last_edited: 12/09/2022 21:38
+ * @last_edited: 4/10/2022 21:38
  */
 
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace BeamMeUpATCA
 {
+    enum TutSeqStage
+    {
+        Welcome,
+        EngineerIntro,
+        HealthIntro,
+        CriticalHealth,
+        RepairProgress,
+        RailFix,
+        MoraleIntro,
+        ScientistIntro,
+        StationingUnit,
+        WeatherStation,
+        EngineerReturn,
+        RadioSignals,
+        Storm,
+        MultiUnitCommand,
+        MoraleLoss,
+        PowerLoss,
+        ScientistObserving,
+        ArrayConfiguation,
+        ScoreIncrease,
+        Overheating,
+        OverheatingFix,
+        End
+    }
+
+    public class PopUpDatum
+    {
+        public int seq_no { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+    }
+
+    public class PopUpRoot
+    {
+        public List<PopUpDatum> popUpDatum { get; set; }
+    }
+
     public class TutorialSequencer : MonoBehaviour
     {
+        // External reference to stored text data
+        [field: SerializeField] public TextAsset jsonData { get; private set; }
         // External references to game objects necessary for tutorial progress.
         [field: SerializeField] public GameObject TutPopUpPrefab { get; private set; }
         [field: SerializeField] public GameObject Player { get; private set; }
@@ -30,6 +73,7 @@ namespace BeamMeUpATCA
         // Position popup directly in centre of screen
         private Vector3 screenCenter = new Vector3(Screen.width/2, Screen.height/2, 0);
         private GameObject currPopUp;
+        private PopUpRoot data;
         private int tutSeqNo;
         private bool popUpActive;
 
@@ -37,7 +81,15 @@ namespace BeamMeUpATCA
         private void Awake() {
             tutSeqNo = 0;
             popUpActive = false;
-            Intro();
+            data = JsonConvert.DeserializeObject<PopUpRoot>(jsonData.text);
+        // PopUpDatum pop = new PopUpDatum();
+        // pop.title = "Hello";
+        // pop.content = "Content";
+        // PopUpRoot popList = new PopUpList();
+        // popList.popUpData = new List<PopUpDatum>();
+        // popList.popUpData.Add(pop);
+        // string jsonString = JsonConvert.SerializeObject(popList);
+        // Debug.LogFormat("Json Doc:" + jsonString);
         }
 
         // Tear down prompts after use
@@ -69,7 +121,7 @@ namespace BeamMeUpATCA
             }
             GameObject newPrompt = Instantiate(TutPopUpPrefab, screenCenter, Quaternion.identity, Canvas.transform);
             // Get references to new text fields and button
-            // Set text and button listner
+            // Set text and button listener
             PopUp popup = newPrompt.GetComponent<PopUp>();
             popup.update_title(title);
             popup.update_content(content);
@@ -78,7 +130,7 @@ namespace BeamMeUpATCA
             popUpActive = true;
         }
         
-        // Resolver for Tutorial Ok clicked
+        // Resolver for Ok Acknowledgement
         private void HandleOk()
         {
             Debug.Log("Button Clicked");
@@ -91,13 +143,19 @@ namespace BeamMeUpATCA
         // Welcome message
         private void Intro()
         {
+            PopUpDatum datum = data.popUpDatum[tutSeqNo];
             Debug.Log("Tutorial Sequence: Intro started");
-            CreateNewPrompt("Welcome to the Beam Me Up ATCA Tutorial",
-                "In this tutorial you will manage the ATCA whilst it performs an observation of" +
-                " Betelguese");
+            CreateNewPrompt(datum.title, datum.content);
         }
 
-        // Introducing engineers
+        // Handles moving to and displaying next tutorial popup
+        private void EnterNextTutPhase()
+        {
+            PopUpDatum datum = data.popUpDatum[tutSeqNo];
+            CreateNewPrompt(datum.title, datum.content);
+        }
+
+        // Introducing engineers (DEPRECATED)
         private void EngineerTut()
         {
             CameraFocus(Engineer);
@@ -124,19 +182,7 @@ namespace BeamMeUpATCA
         {
             if (!popUpActive)
             {
-                switch (tutSeqNo)
-                {
-                    case 0:
-                        Intro();
-                        break;
-                    case 1:
-                        EngineerTut();
-                        break;
-                    case 2:
-                        ResetCamera();
-                        tutSeqNo++;
-                        break;
-                }
+                EnterNextTutPhase();
             }
             else
             {
