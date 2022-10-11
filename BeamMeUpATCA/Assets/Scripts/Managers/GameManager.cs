b55cc09967a3d837944ada2e5c2a48db1b1e6e5d
Jayden Howard <jayden.howard@uqconnect.edu.au>
2022-10-11 01:29:56 +0000

    Global GameManager Singleton.

---
+++ BeamMeUpATCA/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace BeamMeUpATCA
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region  Singleton Management
+        [SerializeField] private bool persistent = false; // Set to true if manager should persist between scenes.
+        
+        private static readonly object ManagerLock = new object();
+
+        private static GameManager _gmInstance;
+        public static GameManager Instance { get
+            {
+                if (!IsQuitting)
+                    lock (ManagerLock)
+                    {
+                        if (!(_gmInstance is null)) return _gmInstance;
+                        GameManager[] gmList = FindObjectsOfType<GameManager>();
+                        
+                        switch (gmList.Length)
+                        {
+                            case 0: // No managers found. Creating and returning new GameObject + instance.
+                                return _gmInstance = new GameObject("GameManager").AddComponent<GameManager>();
+                            case 1: // 1 Manager found. Returning found manager.
+                                return _gmInstance = gmList[0];
+                            default:
+                                Debug.LogWarning("{gmList.Length} GameManagers were found. Returning first instance. Destroying all other instances");
+                                // Destroy all other GameManager objects except first
+                                for (int i = 1; i < gmList.Length; i++) Destroy(gmList[i]);
+
+                                // Return first GameManager object
+                                return _gmInstance = gmList[0];
+                        }
+                    }
+                Debug.LogWarning("Not returning GameManager instance as this application is quitting.");
+                return null;
+            }
+        }
+
+        public static bool IsQuitting { get; private set; }
+        private void OnApplicationQuit() { IsQuitting = true; }
+        #endregion // End of 'Singleton Management'
+
+        // Returns the first player found and sets the private representation of player to match.
+        // If this system was to support multiplayer searches for players would have to be indexed
+        private Player _player;
+        public Player Player => _player ??= FindObjectsOfType<Player>()[0];
+        // Shortcut to get GameManager's active player. Again if multiple player exist this would break.
+        // Systems would have to get UI for the correct player object.
+        public PlayerUI UI => Player.UI; 
+        private void Awake()
+        {
+            // For singleton management
+            if (persistent)DontDestroyOnLoad(gameObject);
+        }
+        
+    }
+}
