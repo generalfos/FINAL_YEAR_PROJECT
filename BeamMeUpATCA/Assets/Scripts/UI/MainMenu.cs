502aa67140937612c214f441b48ab39c5d27e515
Jackson Anthon <jackson@anthon.net>
2022-10-26 18:17:29 +1000

    Hotfix to Menu button and crashing on multiclicking into a scene

--- BeamMeUpATCA/Assets/Scripts/UI/MainMenu.cs
+++ BeamMeUpATCA/Assets/Scripts/UI/MainMenu.cs
@@ -7,6 +7,8 @@ namespace BeamMeUpATCA
 {
     public class MainMenu : MonoBehaviour
     {
+        static bool notPlaying = true;
+        static bool mainMenu = true;
         public void Awake()
         {
             Screen.SetResolution(1920, 1080, true);
@@ -17,12 +19,30 @@ namespace BeamMeUpATCA
 
         public static void Play(int sceneIndex)
         {
-            SceneManager.LoadSceneAsync(sceneIndex);
+            if(notPlaying) {
+                SceneManager.LoadSceneAsync(sceneIndex);
+                notPlaying = false;
+                mainMenu = true;
+            }
+        }
+
+        public static void MainMenuButton() 
+        {
+            if(mainMenu) {
+                notPlaying = true;
+                SceneManager.LoadSceneAsync(0);
+                mainMenu = false;
+            }          
         }
         
         public static void Play(string sceneName)
         {
-            SceneManager.LoadSceneAsync(sceneName);
+            bool notPlaying = true;
+            if(notPlaying) {
+                SceneManager.LoadSceneAsync(sceneName);
+                notPlaying = false;
+                mainMenu = true;
+            }
         }
         
         public static void SetVolume(float volumeAmount)
